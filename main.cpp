1428d9fd4540e108a5c85f2e516fec9eea57c5c4
fcfiallos <fcfiallos@uce.edu.ec>
2025-11-11 16:40:48 -0500

    usando el bin2header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fmt/core.h>
 #include <SFML/Graphics.hpp>
-
+#include "arial.ttf.h"
 int main()
 {
     // std::cout<<"Hello,Fractal Julia" << std::endl;
@@ -12,8 +12,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
     //Create a graphical text to display
-    const sf::Font font("arial.ttf");
-    //const sf:: Font font (arial_ttf,sizeof(Roboto-Italic-VariableFont_wdth,wght_ttf));
+    //const sf::Font font("arial.ttf");
+    const sf:: Font font (arial_ttf, sizeof(arial_ttf));
     sf::Text text(font, "Hello SFML", 50);
     // Start the game loop
     while (window.isOpen())
